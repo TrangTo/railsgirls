a9adad2d5be3417acc6f7339c5b273e73e4db64e
trangto <123.trangto@gmail.com>
2017-09-17 02:56:31 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916070221) do
+ActiveRecord::Schema.define(version: 20170917023416) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "like_count"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
